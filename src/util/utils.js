23d8687b3e8d7e34b4b434d38b514b02c9b4d2a1
waygc <hzwaygc@gmail.com>
2021-03-04 10:27:32 +0800

    添加工具类

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,3 +1,7 @@
+const separator = (require("os").type().toLocaleLowerCase().indexOf("windows") >= 0) ?
+	"\\" :  "/";
+
+
 function a(e) {
 	return 10 > e ? "0" + e : "" + e;
 }
@@ -6,4 +10,8 @@ exports.secondToTime = function(e) {
 	let t = parseInt(e / 60),
 		i = parseInt(e - 60 * t);
 	return a(t) + ":" + a(i);
+}
+
+exports.getSeparator = () => {
+	return separator;
 }
