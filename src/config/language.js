9344d3def4cc41ec74138266c9aad1536dbf4bfa
hzwaygc <hzwaygc@gmail.com>
2021-03-07 03:49:11 +0800

    修改语言配置

--- src/config/language.js
+++ src/config/language.js
@@ -5,12 +5,12 @@ const langFolder = fs.readdirSync(`${global.rootPath}res/languages`);
 const languages = {};
 
 langFolder.map(file => {
-	let fn = file.replace(".json", "");
-	languages[fn] = JSON.parse(fs.readFileSync(`${global.rootPath}res/languages/${file}`, "utf-8"));
+	let name = file.replace(".json", "");
+	languages[name] = JSON.parse(fs.readFileSync(`${global.rootPath}res/languages/${file}`, "utf-8"));
 });
 
 module.exports = exports = {
-	getConfig: () => {
-		
+	getConfig: (name) => {
+		return languages[name];
 	}
 }
