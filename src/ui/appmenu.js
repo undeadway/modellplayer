1916268b54e44f1f9eb8d9751f8bd879f05a718c
waygc <hzwaygc@gmail.com>
2021-03-09 13:31:42 +0800

    修改移动文件后的导入

--- src/ui/appmenu.js
+++ src/ui/appmenu.js
@@ -1,6 +1,6 @@
 // const { Menu,shell,ipcMain,BrowserWindow,app } =require('electron');
 const { shell } =require('electron');
-const dialog = require("./../logic/dialog");
+const dialog = require("./dialog");
 const actions = require("./../logic/actions");
 
 module.exports = exports = {
