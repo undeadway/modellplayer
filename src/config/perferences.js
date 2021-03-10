6c69ecf52262e931bba4e93936624e21228fb1d0
waygc <hzwaygc@gmail.com>
2021-03-10 12:45:16 +0800

    添加避免打包后无法找到文件的路径

--- src/config/perferences.js
+++ src/config/perferences.js
@@ -1,6 +1,6 @@
 const PERFERENCES_NAME = ".perferences";
 const fs = require("fs");
-const perferences = JSON.parse(fs.readFileSync(PERFERENCES_NAME, "utf-8"));
+const perferences = JSON.parse(fs.readFileSync(`${global.rootPath}${PERFERENCES_NAME}`, "utf-8"));
 
 exports = module.exports = {
 	get: () => {
@@ -8,6 +8,6 @@ exports = module.exports = {
 	},
 	write: () => {
 		let string = JSON.stringify(perferences);
-		fs.writeFileSync(PERFERENCES_NAME, string, "utf-8");
+		fs.writeFileSync(`${global.rootPath}${PERFERENCES_NAME}`, string, "utf-8");
 	}
 };
