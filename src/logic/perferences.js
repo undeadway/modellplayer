ada20295444d317d200bb0c6748fa0b6ad1ad803
waygc <hzwaygc@gmail.com>
2021-03-11 15:30:53 +0800

    修改首选项页面处理

--- src/logic/perferences.js
+++ src/logic/perferences.js
@@ -1,15 +1,20 @@
 const $ = require("jquery");
-const { shell } =require('electron').remote;
+const { remote, ipcRenderer } = require('electron');
 require("./../ui/language").init($, "perferences");
 const PerferencesConfig = global.PerferencesConfig ? global.PerferencesConfig : require('electron').remote.getGlobal("PerferencesConfig");
 
 const Logic = {
 	init: () => {
 
-		const { language, about } = PerferencesConfig.get();
+		const perferences = PerferencesConfig.get();
+		const { language, about, setting } = perferences;
+		const newObj = Object.assign({}, perferences);
 
 		const langsSelect = $("#langs-select");
 		const follows = $("#follows");
+		const okBtn = $("#ok-btn");
+		const applyBtn = $("#apply-btn");
+		const cancelBtn = $("#cancel-btn");
 
 		for (let name in language.list) {
 			console.log(name, language.list[name]);
@@ -20,16 +25,63 @@ const Logic = {
 			langsSelect.append(option);
 		}
 
+		for (let name in setting) {
+			let obj = setting[name];
+			if (obj.type !== 'html') {
+				$(`#${name}`).attr(obj.type, obj.value);
+			} else {
+				$(`#${name}`).html(obj.value);
+			}
+		}
+
+		langsSelect.on("change", (evt) => {
+			newObj.language.default = langsSelect.val();
+		});
+
 		about.author.map(obj => {
 			let liObj = $(`<li title="${obj.label}"></li>`);
 			liObj.css({
 				"background-position-x": "-" + (40 * obj.left) + "px"
 			});
 			liObj.on("click", () => {
-				shell.openExternal(obj.link);
+				remote.shell.openExternal(obj.link);
 			});
 			follows.append(liObj);
 		});
+
+		function apply () {
+
+			let setting = newObj.setting;
+
+			for (let name in setting) {
+				let obj = setting[name];
+				if (obj.type !== 'html') {
+					setting[name] = {
+						type: obj.type,
+						value: $(`#${name}`).attr(obj.type)
+					};
+				} else {
+					setting[name] = {
+						type: obj.type,
+						value: $(`#${name}`).html()
+					};
+				}
+			}
+
+			perferences = newObj;
+			PerferencesConfig.write(newObj);
+		}
+
+		function close() {
+			ipcRenderer.send('closePerferencesWindow');
+		}
+
+		applyBtn.on("click", apply);
+		okBtn.on("click", () => {
+			apply();
+			close();
+		})
+		cancelBtn.on("click", close);
 
 	}
 };
