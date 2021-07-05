46e30b6507cb1fcb450a90d7140e95937884fefb
waygc <hzwaygc@gmail.com>
2021-07-05 17:33:36 +0800

    添加 appimage 的打包方式

---
+++ creae-appimage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd dist
+mv ModellPlayer-linux-x64/ bin
+mkdir ModellPlayer-linux-x64
+mv bin ModellPlayer-linux-x64/
+cp -r ../appimage/* ModellPlayer-linux-x64/
+
+appimagetool ModellPlayer-linux-x64
+
+chmod +x ModellPlayer-x86_64.AppImage
+./ModellPlayer-x86_64.AppImage
